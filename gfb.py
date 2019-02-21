938dc3fa7023be078e8909a98b94de1dfa6802e3
montypython <2montypython@gmx.com>
2019-02-21 21:26:24 +0200

    strings repr their chromatic scale

--- gfb.py
+++ gfb.py
@@ -3,49 +3,73 @@ from bestia.output import Row, FString, echo
 from notes import *
 from scales import *
 
-class Fret(object):
-
-    def __init__(self, note='', fret=12):
-        self.fret = fret
-        self.note = Note(' ', ' ', ' ') if not note else note
-
-    def __repr__(self):
-        f = '||' if self.fret == 0 else '|'
-        fret = FString(
-            '{}{}{} {}'.format(self.note.tone, self.note.alt, self.note.octave, f),
-            size=6, align='r', colors=['red']
-        )
-        return str(fret)
-
-
 class String(object):
 
-    fret = []
+    # fret = [] # WHY IS THIS SHARED BETWEEN MY STRING OBJECTS???
 
-    def __init__(self, open_note='Abb3'):
-
-        self.init_frets()
-
-        tone = open_note[0]
-        alt = open_note[1:-1]
-        octave = int(open_note[-1])
+    def __init__(self, tone, alt='', octave=0):
 
         open_note = Note(tone, alt, octave)
-        self.fret[0] = Fret(open_note, fret=0)
+        scale_generator = ChromaticScale(
+            open_note.tone,
+            open_note.alt
+        ).spell()
 
-    def init_frets(self):
-        for n in range(12):
-            self.fret.append(Fret())
-
+        self.fret = []
+        for degree in scale_generator:
+            self.fret.append(degree)
 
     def __repr__(self):
         string_line = Row()
-        for i in range(12):
-            string_line.append(self.fret[i])
+
+        for fret, note in enumerate(self.fret):
+
+            sz = 8 if fret == 0 else 6
+            note = FString(
+                '{}{}{}'.format(note.tone, note.alt, note.octave),
+                size=sz, align='cr', colors=['blue']
+            )
+
+            string_line.append(note)
+
+            sep = FString(
+                '|' if fret == 0 else '¦'
+            )
+            string_line.append(sep)
+        
         return str(string_line)
 
 
 class Tuning(object):
+
+    tuners = '         '
+    # tuners = 'O  O  O  '
+
+    longness = 83
+
+    @classmethod
+    def binding(cls, side='lower'):
+        _binding = '_' if side == 'upper' else '‾'
+        echo(cls.tuners + _binding * cls.longness)
+
+    @classmethod
+    def fret_markers(cls):
+        r = Row(
+            FString('', size=9, pad='*'),
+            FString('I', size=7, align='cl', pad='-'),
+            FString('II', size=7, align='cl'),
+            FString('III', size=7, align='cl'),
+            FString('IV', size=7, align='cl'),
+            FString('V', size=7, align='cl'),
+            FString('VI', size=7, align='cl'),
+            FString('VII', size=7, align='cl'),
+            FString('VIII', size=7, align='cl'),
+            FString('IX', size=7, align='cl'),
+            FString('X', size=7, align='cl'),
+            FString('XI', size=7, align='cl'),
+            FString('XII', size=7, align='cl'),
+            width=len(cls.tuners) +cls.longness
+        ).echo()
 
     def __init__(self, *arg, **kwargs):
         for k, v in kwargs.items():
@@ -55,8 +79,6 @@ class Tuning(object):
 
 def unit_test(s):
     for _row in NOTE_MATRIX:
-        echo(_row, 'green')
-
         for _enharmonic_note in _row:
             if len(_enharmonic_note.alt) >1 or '?' in _enharmonic_note.alt:
                 continue
@@ -75,15 +97,33 @@ if __name__ == '__main__':
         # d_minor = HarmonicMinorScale('D')
         # print(d_minor)
 
-        std_tuning = Tuning(
-            string1='E4',
-            string2='B3',
-            string3='G3',
-            string4='D3',
-            string5='A2',
-            string6='E2'
-        )
+        # std_tuning = Tuning(
+        #     string1='E4',
+        #     string2='B3',
+        #     string3='G3',
+        #     string4='D3',
+        #     string5='A2',
+        #     string6='E2'
+        # )
         # echo(std_tuning.string5)
+
+        Tuning.fret_markers()
+        Tuning.binding('upper')
+
+        s = String('E', '', 4)
+        echo(s, 'green')
+        d = String('B', 'bb', 3)
+        echo(d, 'green')
+        s = String('G', '', 3)
+        echo(s, 'green')
+        s = String('D', '', 3)
+        echo(s, 'green')
+        s = String('A', '', 2)
+        echo(s, 'green')
+        s = String('E', '', 2)
+        echo(s, 'green')
+
+        Tuning.binding('lower')
 
         # empty_fret = Fret(fret=6)
         # string1 = Row(
@@ -114,11 +154,11 @@ if __name__ == '__main__':
         #     width=72
         # ).echo()
 
-        Bb3 = Note('B', 'b', 3)
+        # Bb3 = Note('B', 'b', 3)
         # new_fret = Fret(Bb3, fret=12)
         # print(new_fret)
 
-        unit_test(ChromaticScale)
+        # unit_test(ChromaticScale)
         # unit_test(MajorScale)
         # unit_test(NaturalMinorScale)
         # unit_test(MelodicMinorScale)
