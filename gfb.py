9e49bdd8bee099c35b50358e584a165525418b5a
montypython <2montypython@gmx.com>
2019-03-03 09:50:37 +0100

    removes ? Note from unit tests

--- gfb.py
+++ gfb.py
@@ -4,49 +4,45 @@ from notes import *
 from scales import *
 from guitars import *
 
-def unit_test(s):
+def unit_test(scale):
+    echo(scale.__class__.__name__, 'magenta')
     for _row in ENHARMONIC_MATRIX:
+        # echo(_row, 'green')
         for _enharmonic_note in _row:
-            if len(_enharmonic_note.alt) >1 or '?' in _enharmonic_note.alt:
+            if len(_enharmonic_note.alt) >1: # ignore double alteration notes
                 continue
             echo(_enharmonic_note, 'blue')
-            echo(s(_enharmonic_note.tone, _enharmonic_note.alt), 'cyan')
+            echo(scale(_enharmonic_note), 'cyan')
+        # echo(_row, 'red')
 
 if __name__ == '__main__':
     try:
-        # a_minor = MelodicMinorScale('A')
-        # print(a_minor)
+        unit_test(ChromaticScale)
+        unit_test(MajorScale)
+        unit_test(NaturalMinorScale)
+        unit_test(MelodicMinorScale)
+        unit_test(HarmonicMinorScale)
+
+        c = Note('c')
+        a3 = Note('A', 3)
+        c_major = MajorScale(c)
+        # for n in c_major.scale(10):
+        #     echo(n, 'cyan')
 
         std_tuning = Guitar(
-            string1=Note('E', '', 4),
-            string2=Note('B', '', 3),
-            string3=Note('G', '', 3),
-            string4=Note('D', '', 3),
-            string5=Note('A', '', 2),
-            string6=Note('E', '', 2),
-            string7=Note('B', '', 2),
+            string1=Note('E', 1),
+            string2=Note('B', 3),
+            string3=Note('G', 3),
+            string4=Note('D', 3),
+            string5=Note('A', 2),
+            string6=Note('E', 2),
+            string7=Note('B', 2),
         )
-
-        c_major = MajorScale('E')
+        std_tuning.fretboard()
         std_tuning.fretboard(scale=c_major)
-        # echo(std_tuning.string(4))
-
-        # unit_test(ChromaticScale)
-        # unit_test(MajorScale)
-        # unit_test(NaturalMinorScale)
-        # unit_test(MelodicMinorScale)
-        # unit_test(HarmonicMinorScale)
-
-        # a_major = MinorScale('A')
-        # for n in a_major.ninth():
-        #     print(n)
-
-        # a = Note('A', octave=1)
-        # b = Note('A', octave=2)
-        # if a == b:
-        #     echo('same')
 
     except KeyboardInterrupt:
         echo()
 
 # IMPLEMENT OCTAVES !!!
+# upperscript for alts ???
