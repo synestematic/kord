321447e7759f39243ccb760bfde1cd11c9d2969d
chiclana <jc@gmx.com>
2019-09-18 17:35:25 +0200

    import individual modules

--- kord/__init__.py
+++ kord/__init__.py
@@ -1,1 +1,10 @@
-from kord import universe
+# from kord import universe
+# from kord import tunings
+# from kord import keys
+# from kord import notes
+# from kord import errors
+from kord.universe import *
+from kord.tunings import *
+from kord.keys import *
+from kord.notes import *
+from kord.errors import *
