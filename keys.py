7b7c22698fc91737575af7ed2379fd54e44882c8
chiclana <jc@gmx.com>
2019-09-18 11:59:44 +0200

    improves degree_calculator for ChromaticKey

--- keys.py
+++ keys.py
@@ -7,7 +7,6 @@ class TonalKey(object):
 
     def __init__(self, tone, alt='', oct=0):
         # Major F##, C## work - G## dies...
-        self.current_oct = 0
         self.root = Note(tone, alt, 0)
 
 
@@ -34,8 +33,6 @@ class TonalKey(object):
         yields Notes for diatonic degrees
         if all is set, Nones are yield for empty semi-tones '''
 
-        # self.current_oct = 0
-
         notes_to_yield = notes if notes else len(self._intervals)
         start_note = start if start else self.root
 
@@ -45,6 +42,15 @@ class TonalKey(object):
 
             # IMMEDIATELY DETERMINE WHETHER YIELD MUST BE ENABLED
             degree = self.degree(d)
+            if not degree:
+                raise InvalidScale(
+                    '{}{} {}'.format(
+                        self.root.tone,
+                        self.root.repr_alt,
+                        self.__class__.__name__,
+                    )
+                )
+
             # echo(degree, mode='raw')
             if not yield_enabled and degree >= start_note:
                 yield_enabled = True
@@ -64,6 +70,39 @@ class TonalKey(object):
                 yield degree
 
             d += 1
+
+
+
+class DiatonicKey(TonalKey):
+
+    def degree(self, d):
+
+        if d == 1:
+            return self.root
+
+        # GET DEGREE's ROOT OFFSETS = OCTAVE + SPARE_STS
+        octs_from_root, spare_sts = divmod(
+            self.interval_from_root(d), OCTAVE
+        )
+
+        # GET DEGREE PROPERTIES FROM ENHARMONIC MATRIX
+        next_degrees = [
+            n for n in looped_list_item(
+                self.root.enharmonic_row + spare_sts,
+                ENHARMONIC_MATRIX
+            ) if n.tone == self.root.adjacent_tone(d -1) # EXPECTED TONE
+        ]
+
+        if len(next_degrees) == 1:
+            deg = next_degrees[0]
+            # if deg.tone == 'C': increase_oct()
+        
+            # RETURN NEW OBJECT, DO NOT CHANGE OCT OF ENHARMONIC_MATRIX ITEM!
+            return Note(
+                deg.tone,
+                deg.alt,
+                octs_from_root if deg.enharmonic_row >= self.root.enharmonic_row else octs_from_root +1
+            )
 
 
 
@@ -88,48 +127,6 @@ class TonalKey(object):
     # def thirteenth(self, root=1):
     #     return self._chord(root, count=7)
 
-
-class DiatonicKey(TonalKey):
-
-    def degree(self, d):
-
-        if d == 1:
-            return self.root
-
-        # GET DEGREE's ROOT OFFSETS = OCTAVE + SPARE_STS
-        octs_from_root, spare_sts = divmod(
-            self.interval_from_root(d), OCTAVE
-        )
-
-        # GET DEGREE PROPERTIES FROM ENHARMONIC MATRIX
-        next_degrees = [
-            n for n in looped_list_item(
-                self.root.enharmonic_row + spare_sts,
-                ENHARMONIC_MATRIX
-            ) if n.tone == self.root.adjacent_tone(d -1) # EXPECTED TONE
-        ]
-
-        if len(next_degrees) == 1:
-
-            deg = next_degrees[0]
-            # large intervals that do not hace C will need to compare last and next
-            # if deg.tone == 'C':
-            #     self.current_oct += 1
-
-            # RETURN NEW OBJECT, DO NOT CHANGE OCT OF ENHARMONIC_MATRIX ITEM!
-            return Note(
-                deg.tone,
-                deg.alt,
-                octs_from_root if deg.enharmonic_row >= self.root.enharmonic_row else octs_from_root +1
-            )
-
-        raise InvalidScale(
-            '{}{} {}'.format(
-                self.root.tone,
-                self.root.repr_alt,
-                self.__class__.__name__,
-            )
-        )
 
 
 
@@ -276,44 +273,48 @@ class ChromaticKey(TonalKey):
         if d == 1:
             return self.root
 
-        row_index = self.root.enharmonic_row + self.interval_from_root(d)
+        # GET DEGREE's ROOT OFFSETS = OCTAVE + SPARE_STS
+        octs_from_root, spare_sts = divmod(
+            self.interval_from_root(d), OCTAVE
+        )
 
-        # better also here to get an "expected_tone"
-
-        #  DO I REALLY NEED THESE 3 CHECKS ?
-
+        # GET DEGREE PROPERTIES FROM ENHARMONIC MATRIX
+        # DO I REALLY NEED THESE 3 CHECKS ?
+        # MATCH ROOT_TONE
         next_degrees = [
-            note for note in looped_list_item(row_index, ENHARMONIC_MATRIX) if note.is_note(self.root, ignore_oct=True)
+            n for n in looped_list_item(
+                self.root.enharmonic_row + spare_sts,
+                ENHARMONIC_MATRIX
+            ) if n.is_note(self.root, ignore_oct=True)
         ]
 
         if not next_degrees:
+            # MATCH ROOT_ALT
             next_degrees = [
-                note for note in looped_list_item(row_index, ENHARMONIC_MATRIX) if note.alt == self.root.alt[:-1]
+                n for n in looped_list_item(
+                    self.root.enharmonic_row + spare_sts,
+                    ENHARMONIC_MATRIX
+                ) if n.alt == self.root.alt[:-1]
             ]
 
             if not next_degrees:
+                # CHOOSE "#" or ""
                 chosen_alt = '#' if self.root.alt == '' else self.root.alt
                 next_degrees = [
-                    note for note in looped_list_item(row_index, ENHARMONIC_MATRIX) if note.alt == chosen_alt
+                    n for n in looped_list_item(
+                        self.root.enharmonic_row + spare_sts,
+                        ENHARMONIC_MATRIX
+                    ) if n.alt == chosen_alt
                 ]
 
         if len(next_degrees) == 1:
 
             deg = next_degrees[0] # got from ENH_MATRIX
+            # if Note(deg.tone, deg.alt) == Note('C'): increase_oct()
 
-            if Note(deg.tone, deg.alt) == Note('C'):
-                self.current_oct += 1
-
-            # init new note, DO NOT change octave of ENHARMONIC_MATRIX note!
+            # RETURN NEW OBJECT, DO NOT CHANGE OCT OF ENHARMONIC_MATRIX ITEM!
             return Note(
-                deg.tone, deg.alt, self.current_oct
+                deg.tone,
+                deg.alt,
+                octs_from_root if deg.enharmonic_row >= self.root.enharmonic_row else octs_from_root +1
             )
-
-        raise InvalidScale(
-            '{}{} {}'.format(
-                self.root.tone,
-                self.root.repr_alt,
-                self.__class__.__name__,
-            )
-        )
-
