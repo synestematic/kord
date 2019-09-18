a395b154ac82312c79b813d518522fe6c29f8a15
chiclana <jc@gmx.com>
2019-09-18 16:39:21 +0200

    adds setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="kord",
+    version="0.1",
+    author="Federico Rizzo",
+    author_email="foo@bar.com",
+    description='a python framework for programming music applications',
+    #long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/synestematic/kord",
+    license="MIT",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3 :: Only",
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: MacOS :: MacOS X",
+        "Topic :: Artistic Sfotware",
+    ],
+    install_requires=[
+        'bestia',
+    ],
+)
