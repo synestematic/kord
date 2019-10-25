502de4215759669abc0e4e6aa8ceca17ea61ba14
chiclana <jc@gmx.com>
2019-10-25 18:27:40 +0200

    removes tests

--- kord/__init__.py
+++ kord/__init__.py
@@ -3,7 +3,7 @@
 # from kord import keys
 # from kord import notes
 # from kord import errors
-from kord.universe import *
+# from kord.universe import *
 from kord.instruments import *
 from kord.keys import *
 from kord.notes import *
