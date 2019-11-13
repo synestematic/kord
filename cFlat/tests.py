8c29edec7387f60da45c16b5bb8d8062a4f4b796
chiclana <jc@gmx.com>
2019-11-13 11:14:01 +0100

    adds 2 major scales to DiatonicTests

--- cFlat/tests.py
+++ cFlat/tests.py
@@ -7,26 +7,52 @@ from errors import *
 
 from uuid import uuid4
 
-class ChromaticKeyTestCase(unittest.TestCase):
+# class ChromaticKeyTestCase(unittest.TestCase):
+
+#     def setUp(self):
+#         self.key = ChromaticKey('C')
+
+#     # def testDegree1(self):
+#     #     assert self.key.degree(1) == self.key[1], 'degree() != __get_item__()'
+#     #     assert self.key.degree(1).is_a('C'), 'diocane'
+
+
+#     def testGen(self):
+#         for i, note in enumerate(self.key.scale()):
+#             print(note, end=' ')
+#         print()
+#         print(self.key)
+
+
+class DiatonicKeysTestCase(unittest.TestCase):
 
     def setUp(self):
-        self.key = ChromaticKey('C')
-
-    # def testDegree1(self):
-    #     assert self.key.degree(1) == self.key[1], 'degree() != __get_item__()'
-    #     assert self.key.degree(1).is_a('C'), 'diocane'
+        self.c_major = MajorKey('C')
 
 
     def testGen(self):
-        for i, note in enumerate(self.key.scale()):
-            print(note)
-        # print(self.key)
 
+        for i, note in enumerate(self.c_major.scale()):
+            print(note, end=' ')
+            i += 1
+            if i == 1:
+                assert note.is_a('C', '', 0)
+            elif i == 2:
+                assert note.is_a('D', '', 0)
+            elif i == 3:
+                assert note.is_a('E', '', 0)
+            elif i == 4:
+                assert note.is_a('F', '', 0)
+            elif i == 5:
+                assert note.is_a('G', '', 0)
+            elif i == 6:
+                assert note.is_a('A', '', 0)
+            elif i == 7:
+                assert note.is_a('B', '', 0)
 
-# class DiatonicKeyTestCase(unittest.TestCase):
+        print()
+        # print(self.c_major)
 
-#     def setUp(self):
-#         self.key = DiatonicKey('C')
 
 #     def testDegree1(self):
 #         assert self.key.degree(1) == self.key[1], 'degree() != __get_item__()'
