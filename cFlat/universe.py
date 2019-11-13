f3a8dc92ab090e558a657dc349c20ab9974f69b6
chiclana <jc@gmx.com>
2019-11-13 13:58:15 +0100

    removes tests that have been placed into unit_testing module

--- cFlat/universe.py
+++ cFlat/universe.py
@@ -4,69 +4,6 @@ from bestia.output import echo
 from cFlat.instruments import *
 
 
-DANGEROUS_NON_EQUALS = (
-    # ''' Used mainly to test B#, Cd, etc... '''
-
-    (Note('C', 'b', 3), Note('B', '#', 3)),
-
-    (Note('C', 'b', 3), Note('B', '#', 3)),
-
-    (Note('E', 'b', 5), Note('D', '#', 4)),
-
-    (Note('B', '', 5), Note('C', 'b', 4)),
-
-    (Note('E', '#', 3), Note('F', '', 4)),
-
-
-    (Note('B', 'b'), Note('C', 'bb')),
-    (Note('A', '#'), Note('C', 'bb')),
-
-    (Note('B'     ), Note('C', 'b')),
-    (Note('A', '##'), Note('C', 'b')),
-
-    (Note('B', '#'), Note('C', '')),
-    (Note('B', '#'), Note('D', 'bb')),
-
-    (Note('B', '##'), Note('C', '#')),
-    (Note('B', '##'), Note('D', 'b')),
-
-
-    # these should eval False OK
-    (Note('A', '#'), Note('B', 'b', 4)),
-    (Note('A', '##'), Note('B', '', 4)),
-    (Note('C', ''), Note('D', 'bb', 4)),
-    (Note('C', '#'), Note('D', 'b', 4)),
-)
-
-
-def notes_are_enharmonic(n1, n2):
-    assert n1 == n2
-    # assert n2 == n1
-
-def notes_not_enharmonic(n1, n2):
-    assert n1 != n2
-    # assert n2 != n1
-
-def notes_subtraction(n1, n2):
-    assert n1 - n2 == 0
-
-
-def notes_arent_enharmonic(notes=[]):
-    for n in notes:
-        notes_not_enharmonic(*n)
-
-
-def all_notes_are_enharmonic():
-    ''' checks notes in same enharmonic row are equals '''
-    for enh_notes in EnharmonicMatrix:
-        for x in range(len(enh_notes)):
-            l = [
-                enh_notes[x], enh_notes[x-1]
-            ]
-            notes_are_enharmonic(*l)
-            notes_subtraction(*l)
-
-
 def check_degree_root_intervals(s):
     for n in range(1, 128):
         a = s.degree_root_interval(n) == s[n] - s[1]
@@ -128,9 +65,6 @@ def key_is_valid(KeyClass):
             )
         line.echo()
 
-notes_arent_enharmonic(DANGEROUS_NON_EQUALS)
-
-all_notes_are_enharmonic()
 
 ### KEY TESTS
 key_is_valid(ChromaticKey)
