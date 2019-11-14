05bbb3808886d9dff6c4d79d60907e4f6df26de5
chiclana <jc@gmx.com>
2019-11-14 11:03:35 +0100

    removes universe

--- cFlat/__init__.py
+++ cFlat/__init__.py
@@ -1,10 +1,8 @@
-# from cFlat import universe
 # from cFlat import instruments
 # from cFlat import keys
 # from cFlat import notes
 # from cFlat import errors
 
-# from cFlat.universe import *
 from cFlat.instruments import *
 from cFlat.keys import *
 from cFlat.notes import *
