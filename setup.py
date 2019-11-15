cc210e58b64a676c035c7d8e4e7a252134a7179a
chiclana <jc@gmx.com>
2019-11-15 16:09:33 +0100

    ver0.8

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="cFlat",
-    version="0.7",
+    version="0.8",
     author="Federico Rizzo",
     author_email="foo@bar.com",
     description='a python framework for programming music applications',
