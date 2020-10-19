27eb2928e3a77fa2cedb2af674181124eff60ffc
Malevo <foo@bar.com>
2020-10-19 09:44:25 +0200

    version 1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="kord",
-    version="0.8",
+    version="1.0",
     author="Federico Rizzo",
     author_email="foo@bar.com",
     description='a python framework for programming music applications',
