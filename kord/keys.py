60a8a485b09955ed97da5411923c462950e71ee2
Julio Sosa <bla@abz.com>
2020-10-30 13:48:18 +0100

    allowed_degrees class method instead of argument note_order, degrees class attribute, Chords as Classes (not key.methods)

--- kord/keys.py
+++ kord/keys.py
@@ -4,6 +4,19 @@ from .notes import *
 
 class TonalKey(object):
 
+    degrees = ()
+
+    @classmethod
+    def allowed_degrees(cls):
+        rs = cls.degrees if cls.degrees else [n+1 for n in range( len(cls.root_intervals) )] 
+        out = []
+        for o in range(MAX_OCT):
+            for r in rs:
+                out.append(
+                    r + len(cls.root_intervals) * o
+                )
+        return out
+
     @classmethod
     def Scale(cls, chr, alt=''):
         return cls(chr, alt).scale
@@ -69,7 +82,7 @@ class TonalKey(object):
         return self.root_intervals[d -1]
 
 
-    def _spell(self, note_count=-1, start_note=None, yield_all=True, degree_order=[]):
+    def _spell(self, note_count=-1, start_note=None, yield_all=True, note_order=[]):
         ''' 
             * returns when note_count == 0
             * positive note_count yields notes exactly
@@ -79,14 +92,17 @@ class TonalKey(object):
         if not start_note:
             start_note = self.root
 
-        if not degree_order:
-            degree_order = [
+        if not note_order:
+            note_order = [
                 n+1 for n in range( len(self.root_intervals) )
             ]
 
-        for note in self._filter_degrees(
-            start_note=start_note, degree_order=degree_order
+        for note in self._solmizate(
+            start_note=start_note
         ):
+        # for note in self._order_notes(
+        #     start_note=start_note, note_order=note_order
+        # ):
             if note_count == 0:
                 return
 
@@ -98,27 +114,30 @@ class TonalKey(object):
                 yield None
 
 
-    def _filter_degrees(self, start_note=None, degree_order=[]):
+    def _order_notes(self, start_note=None, note_order=[]):
         ''' 
-            * enforces degree_order for chords, modes, etc
-            * yields None when received note is NOT in degrees
+            * enforces note_order for chords, modes, etc
+            * yields None when received note is NOT in ordered_notes
         '''
-        degrees = [ self[n] for n in degree_order ]
+        ordered_notes = [ self[n] for n in note_order ]
+        # ordered_notes = [ self[n] for n in self.allowed_degrees() ]
 
-        for _, note in self._solmizate(start_note=start_note):
+        for received in self._solmizate(start_note=start_note):
 
             # if solmizate sends a None, yield it immediately
-            if note == None:
+            if received == None:
                 yield None
                 continue
 
-            # if solmizate sends a note, check if it's in degrees and
+            # yield received
+
+            # if solmizate sends a note, check if it's in ordered_notes and
             # if not, yield a None
             # ensures correct amount of non-diatonic Nones for chords
             note_or_none = None
-            for deg in degrees:
-                if note ** deg:
-                    note_or_none = note
+            for on in ordered_notes:
+                if received ** on:
+                    note_or_none = received
                     break
 
             yield note_or_none
@@ -126,9 +145,40 @@ class TonalKey(object):
 
     def _solmizate(self, start_note):
         '''
+        * yields based on allowed_degrees
+        * always yields Nones
+        * changes octs by using self[d]
+        * start_note (diatonic, non-diatonic)
+        '''
+
+        degrees = self.allowed_degrees()
+
+        for d, _ in enumerate(degrees):
+
+            this = self[ degrees[d] ]
+
+            if this < start_note:
+                ## start_note not yet reached
+                continue
+
+            # dont yield the Nones before the starting note
+            if this != start_note:
+                # yield Nones non-diatonic semitones
+                prev = self[ degrees[d-1] ]
+                last_interval = this - prev
+                for semitone in range(last_interval - 1): # dont yield last st, yield the note below
+                    yield None
+
+            yield this
+
+
+
+
+    def _solmizate_orig(self, start_note):
+        '''
         * yields forever
         * always yields 
-        * yields degree number
+        * yields note number
         * changes octs by using self[d]
         * start_note (diatonic, non-diatonic)
         '''
@@ -140,6 +190,10 @@ class TonalKey(object):
                 ## start_note not yet reached
                 continue
 
+            # if self[d] == None:
+            #     yield d, None
+            #     continue
+
             # dont yield the Nones before the starting note
             if self[d] != start_note:
                 # yield Nones non-diatonic semitones
@@ -150,10 +204,13 @@ class TonalKey(object):
             yield d, self[d]
 
 
-    def scale(self, note_count, start_note=None, yield_all=True):
+
+    def scale(self, note_count=None, start_note=None, yield_all=True):
+        if note_count == None:
+            note_count = len(self.root_intervals)
         return self._spell(
             note_count=note_count, start_note=start_note,
-            yield_all=yield_all, degree_order=range(1, len(self.root_intervals) +1),
+            yield_all=yield_all, note_order=range(1, len(self.root_intervals) +1),
         )
 
 
@@ -172,6 +229,9 @@ class DiatonicKey(TonalKey):
         if d < 1:
             return
 
+        if self.degrees and d not in self.allowed_degrees():
+            return
+
         if d == 1:
             return self.root
 
@@ -179,57 +239,26 @@ class DiatonicKey(TonalKey):
         octs_from_root, spare_sts = divmod(
             self.degree_root_interval(d), OCTAVE
         )
-        deg_oct = octs_from_root
+        note_oct = octs_from_root
 
         # GET DEGREE PROPERTIES FROM ENHARMONIC MATRIX
-        next_degrees = [
+        next_notes = [
             n for n in EnharmonicMatrix[
                 self.root.enharmonic_row + spare_sts
-            ] if n.chr == self.root.adjacent_chr(d -1) # EXPECTED TONE
+            ] if n.chr == self.root.adjacent_chr(d - 1) # EXPECTED TONE
         ]
 
-        if len(next_degrees) == 1:
-            deg = next_degrees[0]
+        if len(next_notes) == 1:
+            deg = next_notes[0]
 
             # AT THIS POINT DEG_OCT CAN EITHER STAY | +1
             if self.root.oversteps_oct(deg):
-                deg_oct += 1
+                note_oct += 1
 
             # RETURN NEW OBJECT, DO NOT CHANGE ENHARMONIC MATRIX ITEM!
-            return Note(deg.chr, deg.alt, deg_oct)
+            return Note(deg.chr, deg.alt, note_oct)
 
         raise InvalidNote
-
-
-    def triad(self, note_count=3+1, start_note=None, yield_all=True):
-        return self._spell(
-            note_count=note_count, start_note=start_note,
-            yield_all=yield_all, degree_order=(1, 3, 5),
-        )
-
-    def seventh(self, note_count=4+1, start_note=None, yield_all=True):
-        return self._spell(
-            note_count=note_count, start_note=start_note,
-            yield_all=yield_all, degree_order=(1, 3, 5, 7),
-        )
-
-    def ninth(self, note_count=5+1, start_note=None, yield_all=True):
-        return self._spell(
-            note_count=note_count, start_note=start_note,
-            yield_all=yield_all, degree_order=(1, 3, 5, 7, 9),
-        )
-
-    def eleventh(self, note_count=6+1, start_note=None, yield_all=True):
-        return self._spell(
-            note_count=note_count, start_note=start_note,
-            yield_all=yield_all, degree_order=(1, 3, 5, 7, 9, 11),
-        )
-
-    def thirteenth(self, note_count=7+1, start_note=None, yield_all=True):
-        return self._spell(
-            note_count=note_count, start_note=start_note,
-            yield_all=yield_all, degree_order=(1, 3, 5, 7, 9, 11, 13),
-        )
 
 
 ########################
@@ -247,6 +276,20 @@ class MajorKey(DiatonicKey):
         MAJOR_SEVENTH,
     )
 
+class MajorPentatonicKey(MajorKey):
+    degrees = (1, 2, 3, 5, 6)
+
+class AugmentedKey(DiatonicKey):
+    root_intervals = (
+        UNISON,
+        MAJOR_SECOND,
+        MAJOR_THIRD,
+        PERFECT_FOURTH,
+        AUGMENTED_SIXTH, # <<<
+        MAJOR_SIXTH,
+        MAJOR_SEVENTH,
+    )
+
 class IonianMode(MajorKey):
     pass
 
@@ -260,6 +303,7 @@ class MixolydianMode(MajorKey):
         MAJOR_SIXTH,
         MINOR_SEVENTH, # <<<
     )
+
 
 class LydianMode(MajorKey):
     root_intervals = (
@@ -286,15 +330,21 @@ class MinorKey(DiatonicKey):
         MINOR_SIXTH,
         MINOR_SEVENTH,
     )
-    
-class MinorPentatonicKey(MinorKey):
+
+class DiminishedKey(DiatonicKey):
     root_intervals = (
         UNISON,
+        MAJOR_SECOND,
         MINOR_THIRD,
         PERFECT_FOURTH,
-        PERFECT_FIFTH,
-        MINOR_SEVENTH,
+        DIMINISHED_FIFTH, # <<<
+        MINOR_SIXTH,
+        DIMINISHED_SEVENTH, # <<<
     )
+
+
+class MinorPentatonicKey(MinorKey):
+    degrees = (1, 3, 4, 5, 7)
 
 class Hokkaido(MinorKey):
     root_intervals = (
@@ -305,10 +355,6 @@ class Hokkaido(MinorKey):
         PERFECT_FIFTH,
         MINOR_SIXTH,
     )
-
-
-class NaturalMinorKey(MinorKey):
-    pass
 
 class MelodicMinorKey(MinorKey):
     root_intervals = (
@@ -357,16 +403,54 @@ class PhrygianMode(MinorKey):
         MINOR_SEVENTH,
     )
 
-class MajorTriad(DiatonicKey):
+class LocrianMode(MinorKey):
     root_intervals = (
         UNISON,
-        # MAJOR_SECOND,
-        MAJOR_THIRD,
+        MINOR_SECOND, # <<<
+        MINOR_THIRD,
         PERFECT_FOURTH,
-        PERFECT_FIFTH,
-        MAJOR_SIXTH,
-        MAJOR_SEVENTH,
+        DIMINISHED_FIFTH, # <<<
+        MINOR_SIXTH,
+        MINOR_SEVENTH,
     )
+
+
+####################
+### TRIAD CHORDS ###
+####################
+
+class MajorTriad(MajorKey):
+    degrees = (1, 3, 5)
+
+class MinorTriad(MinorKey):
+    degrees = (1, 3, 5)
+
+class AugmentedTriad(AugmentedKey):
+    degrees = (1, 3, 5)
+
+class DiminishedTriad(DiminishedKey):
+    degrees = (1, 3, 5)
+
+
+######################
+### SEVENTH CHORDS ###
+######################
+
+class DominantSeventhChord(MixolydianMode):
+    degrees = (1, 3, 5, 7)
+
+class MajorSeventhChord(IonianMode):
+    degrees = (1, 3, 5, 7)
+
+class MinorSeventhChord(AeolianMode):
+    degrees = (1, 3, 5, 7)
+
+class HalfDiminishedSeventhChord(LocrianMode):
+    degrees = (1, 3, 5, 7)
+
+class DiminishedSeventhChord(DiminishedKey):
+    degrees = (1, 3, 5, 7)
+
 
 #####################
 ### CHROMATIC KEY ###
@@ -401,54 +485,53 @@ class ChromaticKey(TonalKey):
         octs_from_root, spare_sts = divmod(
             self.degree_root_interval(d), OCTAVE
         )
-        deg_oct = octs_from_root
+        note_oct = octs_from_root
 
         # GET DEGREE PROPERTIES FROM ENHARMONIC MATRIX
         # DO I REALLY NEED THESE 3 CHECKS ?
         # MATCH ROOT_TONE
-        next_degrees = [
+        next_notes = [
             n for n in EnharmonicMatrix[
                 self.root.enharmonic_row + spare_sts
             ] if n ** self.root
         ]
 
-        if not next_degrees:
+        if not next_notes:
             # MATCH ROOT_ALT
 
-            next_degrees = [
+            next_notes = [
                 n for n in EnharmonicMatrix[
                     self.root.enharmonic_row + spare_sts
                 ] if n.alt == self.root.alt[:-1]
             ]
 
-            if not next_degrees:
+            if not next_notes:
                 # CHOOSE "#" or ""
                 chosen_alt = '#' if self.root.alt == '' else self.root.alt
-                next_degrees = [
+                next_notes = [
                     n for n in EnharmonicMatrix[
                         self.root.enharmonic_row + spare_sts
                     ] if n.alt == chosen_alt
                 ]
 
-        if len(next_degrees) == 1:
+        if len(next_notes) == 1:
 
-            deg = next_degrees[0] # got from ENH_MATRIX
+            deg = next_notes[0] # got from ENH_MATRIX
 
             # AT THIS POINT DEG_OCT CAN EITHER STAY | +1
             if self.root.oversteps_oct(deg):
-                deg_oct += 1
+                note_oct += 1
 
             # RETURN NEW OBJECT, DO NOT CHANGE ENHARMONIC MATRIX ITEM!
-            return Note(deg.chr, deg.alt, deg_oct)
+            return Note(deg.chr, deg.alt, note_oct)
 
         raise InvalidNote
 
 SCALES = {
 
     'major': MajorKey.Scale,
-
     'minor': MinorKey.Scale,
-    'natural_minor': NaturalMinorKey.Scale,
+
     'melodic_minor': MelodicMinorKey.Scale,
     'harmonic_minor': HarmonicMinorKey.Scale,
 
