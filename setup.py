3f9e3f0c5253e7194dfccb705e06f7aac391b060
Julio Sosa <bla@abz.com>
2020-11-02 23:21:46 +0100

    version 2.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="kord",
-    version="2.5",
+    version="2.6",
     author="Federico Rizzo",
     author_email="synestem@tic.com",
     description='a python framework for programming music applications',
