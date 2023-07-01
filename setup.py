cf75c4a719e06185982bedc8bed51b99e13021a9
Federico Rizzo <synestematic@gmail.com>
2023-07-01 02:47:53 +0200

    version 3.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="kord",
-    version="3.5",
+    version="3.6",
     author="Federico Rizzo",
     author_email="synestem@ticATgmail.com",
     description='a framework for programming music applications',
