735e99dec1c1e4286d6e5345eb75916d023bf0a1
Federico Rizzo <synestematic@gmail.com>
2024-12-27 15:44:37 +0100

    creates parsers package

--- kord/parsers/chord_parser.py
+++ kord/parsers/chord_parser.py
@@ -7,7 +7,6 @@ from .errors import InvalidNote, InvalidAlteration, InvalidOctave, InvalidChord
 from bestia.output import echo
 
 __all__ = [
-    'NotePitchParser',
     'ChordParser',
 ]
 
@@ -128,81 +127,4 @@ class ChordParser:
             # init instance of Chord class using Chord root
             return self.flavor(*self.root)
         return None
-
-
-class NotePitchParser:
-
-    def __init__(self, symbol):
-        self.symbol = symbol
-        self.reset()
-
-
-    def reset(self):
-        self.to_parse = self.symbol.strip()
-
-
-    def _parse_char(self):
-        char = NotePitch.validate_char(self.to_parse[0])
-        self.to_parse = self.to_parse[1:]
-        return char
-
-
-    def _parse_oct(self):
-        if len(self.to_parse) > 1:
-            try:
-                # this being an int means octave > MAXIMUM_OCTAVE
-                octave = int(self.to_parse[-2])
-                octave_over_max = True
-            except ValueError:
-                # probably an alt
-                octave_over_max = False
-            finally:
-                if octave_over_max:
-                    raise InvalidOctave(self.symbol)
-
-        try:
-            # this being an int means this is an octave
-            octave = int(self.to_parse[-1])
-            self.to_parse = self.to_parse[:-1]
-        except ValueError:
-            # probably an alt
-            octave = NotePitch.DEFAULT_OCTAVE
-        finally:
-            return octave
-
-
-    def _parse_alts(self):
-        self.to_parse = self.to_parse.replace('𝄫', 'bb')
-        self.to_parse = self.to_parse.replace('♭', 'b')
-        self.to_parse = self.to_parse.replace('𝄪', '##')
-        self.to_parse = self.to_parse.replace('♯', '#')
-
-        if self.to_parse not in NotePitch.input_alterations():
-            raise InvalidAlteration(self.symbol)
-
-        alts = self.to_parse
-        self.to_parse = self.to_parse[len(self.to_parse):]
-        return alts
-
-
-    def parse(self):
-        if len(self.to_parse) == 0:
-            raise InvalidNote(self.symbol)
-
-        char = self._parse_char()
-        if len(self.to_parse) == 0:
-            self.reset()
-            return NotePitch(char)
-
-        octave = self._parse_oct()
-        if len(self.to_parse) == 0:
-            self.reset()
-            return NotePitch(char, octave)
-
-        alts = self._parse_alts()
-        if len(self.to_parse) == 0:
-            self.reset()
-            return NotePitch(char, alts, octave)
-
-        raise InvalidNote(self.symbol)
 
