33cf7fa71dae857e8f98478e3e03910ede958b50
Federico Rizzo <synestematic@gmail.com>
2024-12-28 14:23:05 +0100

    adds naturals module

--- kord/notes/__init__.py
+++ kord/notes/__init__.py
@@ -1,19 +1,9 @@
-from bestia.iterate import LoopedList
 
 from .intervals import Intervals
 from .pitch import *
 
-from ..errors import InvalidNote, InvalidAlteration, InvalidOctave
+from .naturals import *
 
 __all__ = [
-    'NotePitch',
+
 ]
-
-# C3 = NotePitch('C', '' , 3)
-# D3 = NotePitch('D', '' , 3)
-# E3 = NotePitch('E', '' , 3)
-# F3 = NotePitch('F', '' , 3)
-# G3 = NotePitch('G', '' , 3)
-# A3 = NotePitch('A', '' , 3)
-# B3 = NotePitch('B', '' , 3)
-
