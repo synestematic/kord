25162653834c8d83544bde56b119c89023c3db3a
Federico Rizzo <synestematic@gmail.com>
2024-12-28 15:14:40 +0100

    import notes only from constants package

--- kord/notes/__init__.py
+++ kord/notes/__init__.py
@@ -2,12 +2,6 @@
 from .intervals import Intervals
 from .pitch import *
 
-from .constants.naturals import *
-from .constants.flats import *
-from .constants.sharps import *
-from .constants.double_flats import *
-from .constants.double_sharps import *
-
 __all__ = [
-
+    # 'NotePitch',
 ]
